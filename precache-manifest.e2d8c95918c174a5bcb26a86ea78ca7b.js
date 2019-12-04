04e124acc5ba9eed76c712e99673531b536d36ba
Michael Knoll <mknoll@fearless.tech>
2019-12-03 23:25:23 -0500

    Updates

---
+++ precache-manifest.e2d8c95918c174a5bcb26a86ea78ca7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc489426b714fe3c45765aa1205a166b",
+    "url": "/ReactMatchingGame/index.html"
+  },
+  {
+    "revision": "b14e8c02dd090eb730b3",
+    "url": "/ReactMatchingGame/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c23d8e7dade5ef241312",
+    "url": "/ReactMatchingGame/static/css/main.2fca0c46.chunk.css"
+  },
+  {
+    "revision": "b14e8c02dd090eb730b3",
+    "url": "/ReactMatchingGame/static/js/2.bbf4e34f.chunk.js"
+  },
+  {
+    "revision": "c23d8e7dade5ef241312",
+    "url": "/ReactMatchingGame/static/js/main.a9eb1f94.chunk.js"
+  },
+  {
+    "revision": "bcde099af48b09b19783",
+    "url": "/ReactMatchingGame/static/js/runtime-main.359dc76c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ReactMatchingGame/static/media/logo.25bf045c.svg"
+  }
+]);
